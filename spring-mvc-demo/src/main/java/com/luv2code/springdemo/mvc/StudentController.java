47528c992825abbc4954f05ba5c4d1b03b5ed5d5
mohamedkhattab2 <mohamed.khattab84@yahoo.com>
2022-09-15 16:55:38 +0200

    add student form and confirmation page

--- spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/StudentController.java
+++ spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/StudentController.java
@@ -1,5 +1,28 @@
 package com.luv2code.springdemo.mvc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
 public class StudentController {
 
+	// create a method for showing the form
+	@RequestMapping("showForm")
+	public String showForm(Model theModel) {
+		// create a new student object
+		Student theStudent = new Student();
+
+		// add student object to the model
+		theModel.addAttribute("student", theStudent);
+
+		return "student-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(@ModelAttribute("student") Student theStudent) {
+		return "student-confirmation";
+	}
 }
---
+++ spring-mvc-demo/src/main/webapp/WEB-INF/view/student-confirmation.jsp
@@ -0,0 +1,17 @@
+
+<DOCTYPE html>
+<html>
+<head>
+<title>Hello World - Input Form</title>
+</head>
+
+<body>
+
+	<h2>Hello world of Spring</h2>
+	<br>
+	<br> Student name: ${param.studentName }
+	<br>
+	<br> The message: ${message}
+
+</body>
+</html>
